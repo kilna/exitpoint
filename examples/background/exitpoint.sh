862b6d667512749c724e365c0e843eea001b564c
Kilna <kilna@kilna.com>
2024-06-08 21:30:23 -0700

    Add examples

---
+++ examples/background/exitpoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "This was run when the container got a termination signal"
+
---
+++ examples/exit-only/exitpoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "This was run when the container got a termination signal $SIGNAL"
+
+echo "Startup command exited with code $CMD_EXIT"
+
