9ad044e4e503e9ea27b8f0de8daf0f360ed3dc37
Kilna <kilna@kilna.com>
2024-06-11 12:34:14 -0700

    Add automatic latest version

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ case "$BASE_IMAGE" in
       curl -sL http://api.github.com/repos/krallin/tini/git/refs/tags \
         jq -crM '.[].ref' | sed -e 's|refs/tags/||' | sort -V | tail -1
     )"
-    TINI_BASE=https://github.com/krallin/tini/releases/download/
+    TINI_BASE=https://github.com/krallin/tini/releases/download
     cd /install/sbin
     wget "$TINI_BASE/$TINI_VERSION/tini-static-muslc-$arch" -O tini \
       || wget "$TINI_BASE/$TINI_VERSION/tini-static-$arch" -O tini
